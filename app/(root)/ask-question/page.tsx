c144534143d6a74459c1ce115ddb267a4c8bea10
ariya das <ariyadas33@gmail.com>
2023-09-29 11:48:39 +0530

    add auth using clerk

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex min-h-screen w-full items-center justify-center'>Ask Question</div>
+  )
+}
+
+export default page
