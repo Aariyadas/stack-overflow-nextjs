2197eadfc46fc58b5238ea13eda1abf6b9367b23
ariya das <ariyadas33@gmail.com>
2023-10-06 22:42:00 +0530

    Ridht side bar done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 
 import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
 import Navbar from '@/components/shared/navbar/Navbar'
 import React from 'react'
 
@@ -14,7 +15,7 @@ const Layout = ({children}:{children:React.ReactNode}) => {
             {children}
           </div>
         </section>
-        RightSider
+        <RightSidebar/>
      </div>
  
     </main>
