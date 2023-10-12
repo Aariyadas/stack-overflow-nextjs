f3a1a8dbf7762987f59d1be6b71f7311e6a87534
ariya das <ariyadas33@gmail.com>
2023-10-12 20:27:56 +0530

    deployment

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-
+import React from 'react';
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex min-h-screen w-full items-center justify-center">
