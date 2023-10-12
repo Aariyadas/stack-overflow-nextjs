0fe6a9e64bb4f09d735abc866d491795887dd535
ariya das <ariyadas33@gmail.com>
2023-10-12 20:15:50 +0530

    deployment issue

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,8 @@
+import React from 'react'
 
 import LeftSidebar from '@/components/shared/LeftSidebar'
 import RightSidebar from '@/components/shared/RightSidebar'
 import Navbar from '@/components/shared/navbar/Navbar'
-import React from 'react'
-
 const Layout = ({children}:{children:React.ReactNode}) => {
   return (
     <main className='background-light850_dark100 relative'>
