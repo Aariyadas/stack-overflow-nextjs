e79c06b43e6c178df6bd3a55272c1ea6ca6685dd
ariya das <ariyadas33@gmail.com>
2023-10-12 20:32:10 +0530

    deploy

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import LeftSidebar from '@/components/shared/LeftSidebar'
 import RightSidebar from '@/components/shared/RightSidebar'
 import Navbar from '@/components/shared/navbar/Navbar'
