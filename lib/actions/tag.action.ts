1102ebaff03c9920755a07564687c30f207e3b1a
ariya das <ariyadas33@gmail.com>
2023-10-14 21:02:53 +0530

    tagpage implemented

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,7 +1,8 @@
 "use server"
 import User from '@/database/user.model';
 import { connectToDatabase } from '../mongoose';
-import { GetTopInteractedTagsParams } from './shared.types.d';
+import { GetAllTagsParams, GetTopInteractedTagsParams } from './shared.types.d';
+import Tag from '@/database/tag.model';
 
 
 export async function getTopInteractedTags(params:GetTopInteractedTagsParams){
@@ -17,6 +18,19 @@ export async function getTopInteractedTags(params:GetTopInteractedTagsParams){
     // Interaction........
     return [{_id:'1',name:'tag1'},
     {_id:'2',name:'tag2'}]
+  }catch(error){
+    console.log(error)
+    throw error
+
+  }
+}
+
+
+export async function getAllTags(params:GetAllTagsParams){
+  try{
+    connectToDatabase();
+    const tags =await Tag.find({});
+    return {tags}
   }catch(error){
     console.log(error)
     throw error
