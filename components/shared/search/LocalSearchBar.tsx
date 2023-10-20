46240fa192d3ba60186c9391fbd740a2e9a64cb8
ariya das <ariyadas33@gmail.com>
2023-10-20 16:03:21 +0530

    Debounce function

--- components/shared/search/LocalSearchBar.tsx
+++ components/shared/search/LocalSearchBar.tsx
@@ -1,9 +1,11 @@
-"use client"
+"use client";
 import { Input } from "@/components/ui/input";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
 import Image from "next/image";
-import React from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
-interface CustomINputProps {
+interface CustomInputProps {
   route: string;
   iconPosition: string;
   imgSrc: string;
@@ -17,25 +19,58 @@ const LocalSearchBar = ({
   imgSrc,
   placeholder,
   otherClasses,
-}: CustomINputProps) => {
+}: CustomInputProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("q");
+  console.log(query);
+  const [search, setSearch] = useState(query || "");
+
+  useEffect(() => {
+    const debounce = setTimeout(() => {
+      if (search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "q",
+          value: search,
+        });
+
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (pathname === route) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysRemove: ["q"],
+          });
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 300);
+
+    return () => clearTimeout(debounce);
+  }, [search, route, pathname, searchParams, router, query]);
+
   return (
     <div
       className={`background-light800_darkgradient flex min-h-[56px]  grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
     >
-        {iconPosition === 'left' && (<Image 
-        src={imgSrc}
-        alt="Search Icon"
-        width={24}
-        height={24}
-        className="cursor-pointer"
+      {iconPosition === "left" && (
+        <Image
+          src={imgSrc}
+          alt="Search Icon"
+          width={24}
+          height={24}
+          className="cursor-pointer"
         />
-        )}
-        <Input
+      )}
+      <Input
         type="text"
         placeholder={placeholder}
-        value=""
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
         className="paragraph-regular no-focus  placeholder background-light800_darkgradient border-none shadow-none outline-none"
-        />
+      />
     </div>
   );
 };
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import qs from "query-string"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -54,5 +55,41 @@ export const  formatNumberWithExtension=(number: number): string =>{
 }
 
 
+interface UrlQueryParams {
+  params:string;
+  key:string;
+  value:string | null;
+
+}
+
+export const formUrlQuery=({params,key,value}:UrlQueryParams) =>{
+  const currentUrl =qs.parse(params);
+  currentUrl[key]=value;
+
+  return qs.stringifyUrl({
+    url:window.location.pathname,
+    query:currentUrl,
+  },{skipNull:true})
+}
+
+
+interface RemoveUrlQueryParams {
+  params:string;
+  keysRemove:string[];
+ 
+
+}
+export const removeKeysFromQuery=({params,keysRemove}:RemoveUrlQueryParams) =>{
+  const currentUrl =qs.parse(params);
+
+  keysRemove.forEach((key)=>{
+    delete currentUrl[key]
+  })
+
+  return qs.stringifyUrl({
+    url:window.location.pathname,
+    query:currentUrl,
+  },{skipNull:true})
+}
 
 
