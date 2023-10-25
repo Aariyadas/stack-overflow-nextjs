a8f6bb578e07143d220c3ee94066e19822158350
ariya das <ariyadas33@gmail.com>
2023-10-25 13:12:38 +0530

    debouncing done global search

---
+++ components/shared/search/GlobalResult.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const GlobalResult = () => {
+  return (
+   
+    <div className="absolute top-full z-1 w-full">
+        Filers
+        <div className='my-5 h-[1px] bg-light-700/50 dark:bg-dark-500/50'>
+wshjdfhweoif
+        </div>
+    </div>
+  )
+}
+
+export default GlobalResult
